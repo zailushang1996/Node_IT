f08b41021e2775ab871f6dba4609ce88a5f5eba3
zailushang1996 <2212926237@qq.com>
2018-07-17 20:05:51 +0800

    模拟express

--- 11router/model/express-route.js
+++ 11router/model/express-route.js
@@ -6,7 +6,7 @@ function changeRes(res){
 
     res.send=function(data){
 
-        res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
+        res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
 
         res.end(data);
     }
